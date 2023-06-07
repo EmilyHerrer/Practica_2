6ab1ccfeafed68aaa48709804de6a8f9b657ac49
emilyherrera <emilyherrera@192.168.0.8>
2023-06-07 10:30:54 -0600

    Creacion de pagina web, Stardew Valley wiki

---
+++ src/main/java/com/Wiki_Stardew_Valley/WikiStardewValleyApplication.java
@@ -0,0 +1,13 @@
+package com.Wiki_Stardew_Valley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiStardewValleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiStardewValleyApplication.class, args);
+	}
+
+}
